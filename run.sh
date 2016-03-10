00cc2936b027b4098f3658e91e8758fbb7ec43cb
santos82 <santos82h@gmail.com>
2016-03-10 23:43:49 +0100

    saltar copia

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-echo -n "Copiando html ... "
-mv ~/Descargas/LFS201*.html html/ori 2> /dev/null
-rm html/clean/* 2> /dev/null
-rm md/* 2> /dev/null
-cp html/ori/* html/clean/
-rename 's/_popup.html$/_popup (0).html/' html/clean/*.html
-echo "ok"
-
+if [ "$1" == "-r" ]; then
+	rm ~/Descargas/LFS201*.html 2> /dev/null
+else
+	echo -n "Copiando html ... "
+	mv ~/Descargas/LFS201*.html html/ori 2> /dev/null
+	rm html/clean/* 2> /dev/null
+	rm md/* 2> /dev/null
+	cp html/ori/* html/clean/
+	rename 's/_popup.html$/_popup (0).html/' html/clean/*.html
+	echo "ok"
+fi
 echo -n "Limpiando html ... "
 python clean.py
 echo "ok"
