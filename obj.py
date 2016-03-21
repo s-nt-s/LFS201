82831cd4076f7ded05570e580e14fde57741ab83
santos82 <santos82h@gmail.com>
2016-03-21 22:51:07 +0100

    objetivos

---
+++ obj.py
@@ -0,0 +1,34 @@
+import bs4
+import re
+
+html="out/LFS201.html"
+out="out/Objetivos.html"
+
+
+def get_soup(html):
+	html = open(html,"r+")
+	soup = bs4.BeautifulSoup(html,'html.parser')#"lxml")
+	html.close()
+	return soup
+
+soup=get_soup(html)
+soup.title.string=soup.title.string+": Objetivos"
+
+flds=soup.findAll("fieldset", attrs={'class': re.compile(r".*\bn2\b.*")})
+for f in flds:
+	if f.legend.get_text().strip().lower()=="objetivos de aprendizaje":
+		f.legend.string=f.parent.h1.a.string
+		f.div.p.extract()
+	else:
+		f.extract()
+
+for h in soup.findAll("h1"):
+	h.extract()
+
+for div in soup.body.div.select(" > div"):
+	div.unwrap()
+
+h=unicode(soup)
+#h=bk.sub("\\n<\\1>",h)
+with open(out, "wb") as file:
+	file.write(h.encode('utf8'))
